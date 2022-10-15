aea9969fe9688d480e8b5dbb86ff8e9656c3dbc8
Jorge Brandon Chandomi Esponda <Sr.conejo.uwu@gmail.com>
2022-10-15 13:57:57 -0500

    message

--- installer/installer.sh
+++ installer/installer.sh
@@ -5,3 +5,5 @@ path="${input:-$path}"
 cp ../source/springEasy.sh $path
 
 echo 'alias spe="'$path'springEasy.sh"' >> ~/.bashrc | source ~/.bashrc
+
+echo "if spe comand doesn't work please run: source ~/.bashrc"
